afba18b258ab01d067db1c3945ab52e93d377b77
Pavel Khokhlov <khokhlovpavel1986@yandex.ru>
2020-06-19 00:11:10 +0300

    Первый шаг

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,1 +1,9 @@
 #!/usr/bin/env node
+import commander from 'commander';
+
+const program = commander;
+
+program
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.')
+  .parse(process.argv);

215819b5e387f3443ff3a32c39e2a4fdbc6b0d2a
Pavel Khokhlov <khokhlovpavel1986@yandex.ru>
2020-08-07 19:42:20 +0300

    Доп исправления после 7-го ревью

--- src/index.js
+++ src/index.js
@@ -4,18 +4,12 @@ import parse from './parsers.js';
 import render from './formatters/index.js';
 import buildTree from './buildTree.js';
 
-/* const getDataAndFormatNameFromFile = (pathToFile) => {
-  const data = fs.readFileSync(path.resolve(process.cwd(), pathToFile), 'utf8');
-  const formatName = path.extname(pathToFile).slice(1);
-  return { data, formatName };
-}; */
-
 const getDataFromFile = (pathToFile) => fs.readFileSync(path.resolve(process.cwd(), pathToFile), 'utf8');
-const getFormatNameFromFile = (pathToFile) => path.extname(pathToFile).slice(1);
+const getFormatName = (pathToFile) => path.extname(pathToFile).slice(1);
 
 export default (pathToFile1, pathToFile2, format) => {
-  const parsedObject1 = parse(getDataFromFile(pathToFile1), getFormatNameFromFile(pathToFile1));
-  const parsedObject2 = parse(getDataFromFile(pathToFile2), getFormatNameFromFile(pathToFile2));
+  const parsedObject1 = parse(getDataFromFile(pathToFile1), getFormatName(pathToFile1));
+  const parsedObject2 = parse(getDataFromFile(pathToFile2), getFormatName(pathToFile2));
   const tree = buildTree(parsedObject1, parsedObject2);
   return render(tree, format);
 };
